63eeda80f10194717d3de16cd78e054d932e995b
Joe Delfino <jdelfino@gmail.com>
2024-01-09 20:48:48 +0000

    bad tests, good coverage

--- index.test.js
+++ index.test.js
@@ -1,11 +1,11 @@
 const countVowels = require("./index");
 
 test("count vowels in a string", () => {
-  expect(countVowels("hello")).toBe(2);
-  expect(countVowels("coding")).toBe(2);
-  expect(countVowels("world")).toBe(1);
+  expect(countVowels("hello")).not.toBe(0);
+  expect(countVowels("coding")).not.toBe(0);
+  expect(countVowels("world")).not.toBe(0);
 });
 
 test("count vowels in non-string is 0", () => {
-  //expect(countVowels(0)).toBe(0);
+  expect(countVowels(0)).toBe(0);
 }) 
